0baf89e6909850e0d8a1d62a7f995b87bb9db016
calihuddleston <calihuddleston@gmail.com>
2022-06-14 22:31:54 -0400

    "Finishing up, added comments, cleaned up code. Attempted to add reset button but failed to do so. "

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,21 +1,20 @@
 // Assignment Code
-let generateBtn = document.querySelector("#generate");
-
-let choices = {
-lowercaseChar: "qwertyuiopasdfghjklzxcvbnm",
-uppercaseChar: "QWERTYUIOPASDFGHJKLZXCVBNM",
-numericChar: "1234567890",
-specialChar: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
-}
-
+  let generateBtn = document.querySelector("#generate");
+  let lowercaseChar = "qwertyuiopasdfghjklzxcvbnm";
+  let uppercaseChar = "QWERTYUIOPASDFGHJKLZXCVBNM";
+  let numericChar = "1234567890";
+  let specialChar = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+  let passwordChar;
+  let password = "";
 
 // Write password to the #password input
 
 function generatePassword() {
-  console.log("you pressed the button!");
+// Prompts user to enter numeric value for password
   let passwordLengthPrompt = window.prompt("How many characters would you like your password to contain? Must be between 8-128.");
   passwordLengthPrompt = parseInt(passwordLengthPrompt);
   
+// Conditional based on password length criteria, if prompt return true a window alert will say 'Great'. If it returns false an alert window will give a reminder of the length criteria and direct them back to the input prompt.
   if (passwordLengthPrompt >= 8 && passwordLengthPrompt <= 128) {
     window.alert("Great!");
 }  else {
@@ -23,56 +22,57 @@ function generatePassword() {
   generatePassword();
 }
 
-let confirmLowercase = confirm("Would you like to include lowercase characters?");
-  if (confirmLowercase) {
-    console.log("include lowercase");
-    (choices ["lowercaseChar"]);
-  } else {
-    console.log("don't include lowercase");
-  } 
+// Confirms if user would like lowercase characters
+  let confirmLowercase = confirm("Would you like to include lowercase characters?");
+    if (confirmLowercase) {
+      console.log("include lowercase");
+      passwordChar += lowercaseChar;
+    } else {
+      console.log("don't include lowercase");
+    } 
 
-let confirmUppercase = confirm ("Would you like to include uppercase characters?");
-  if (confirmUppercase){
-      console.log("include uppercase");
-      (choices ["uppercaseChar"]);
-  } else {
-      console.log("don't include uppercase");
-  }
-
-let confirmNumeric = confirm("Would you like to include numbers?");
-  if (confirmNumeric){
-      console.log("include numbers");
-      (choices ["numericChar"])
-  } else {
-      console.log ("don't include numbers");
+// Confirms if user would like uppercase characters 
+  let confirmUppercase = confirm ("Would you like to include uppercase characters?");
+    if (confirmUppercase){
+        console.log("include uppercase");
+        passwordChar += uppercaseChar;
+    } else {
+        console.log("don't include uppercase");
     }
 
-let confirmSpecialChar = confirm ("Would you like to include special characters?")
-  if (confirmSpecialChar) {
-      console.log("include special characters");
-      (choices ["specialChar"]);
-  } else {
-      console.log("don't include special characters")
+// Confirms if user would like numerical characters
+  let confirmNumeric = confirm("Would you like to include numbers?");
+    if (confirmNumeric){
+        console.log("include numbers");
+        passwordChar += numericChar;
+    } else {
+        console.log ("don't include numbers");
+      }
+
+// Confirms if user would like special characters
+  let confirmSpecialChar = confirm ("Would you like to include special characters?")
+    if (confirmSpecialChar) {
+        console.log("include special characters");
+        passwordChar += specialChar;
+    } else {
+        console.log("don't include special characters")
+    }
+
+// Generates password based on criteria 
+  for (let i = 0; i < passwordLengthPrompt; i++) {
+    password += passwordChar[Math.floor(Math.random() * passwordChar.length)]
   }
-// 1. Prompts the user for the password criteria
-//    a. password length 8 < 128
-//    b. lowercase, uppercase, numbers, special characters
-// 2. Validate input
-// 3. Generate password based on criteria 
-for (let i = 0; i < passwordLengthPrompt; i++) {
-  password = choices[Math.floor(Math.random() * choices.length)]
-}
-// 4. Display password to the page
-return password
+
+// Displays password to the page
+  return password;
 } 
 
 function writePassword() {
-  let password = generatePassword();
+   password = generatePassword();
   let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Adds event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
